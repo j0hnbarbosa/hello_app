115b98cb65a26dd429f82f1ffb64a47f0a87ba5a
John Barbosa <john@smtsolucoes.com.br>
2019-06-26 14:21:49 -0300

    Create page 'new'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#goodbye'
+  root 'welcome#index'
 end
